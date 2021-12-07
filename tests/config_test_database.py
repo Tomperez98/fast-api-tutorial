024973657fbf7196ce0142b6d87fe1fb94ac2eee
Tomperez98 <tomasperezalvarez@gmail.com>
2021-12-06 21:01:54 -0500

    Min 15:40:32 test database that destroys itself after each test is already setup

---
+++ tests/config_test_database.py
@@ -0,0 +1,31 @@
+from src import database, main
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+main.app.dependency_overrides[database.get_db] = override_get_db
+
+
+@pytest.fixture
+def client():
+    database.Base.metadata.create_all(bind=engine)
+    yield TestClient(main.app)
+    database.Base.metadata.drop_all(bind=engine)
