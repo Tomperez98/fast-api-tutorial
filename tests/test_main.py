ccc585fb3b03dce4e323554463d226d09ef4fea6
Tomperez98 <tomasperezalvarez@gmail.com>
2021-12-06 21:05:41 -0500

    Small change in folder structure

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
 from tests import config_test_database
-from src.schemas.response import response_user
 
 test_client = config_test_database.client
 
@@ -8,14 +7,3 @@ def test_root(test_client):
     response = test_client.get("/")
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
-
-
-def test_create_user(test_client):
-    response = test_client.post(
-        url="/users/", json={"email": "nuevaao@gmail.com", "password": "123"}
-    )
-
-    new_user = response_user.CreatedUser(**response.json())
-    assert response.status_code == 201
-    assert new_user.email == "nuevaao@gmail.com"
-    assert new_user.id == 1
---
+++ tests/test_routers/test_router_users.py
@@ -0,0 +1,15 @@
+from tests import config_test_database
+from src.schemas.response import response_user
+
+test_client = config_test_database.client
+
+
+def test_create_user(test_client):
+    response = test_client.post(
+        url="/users/", json={"email": "nuevaao@gmail.com", "password": "123"}
+    )
+
+    new_user = response_user.CreatedUser(**response.json())
+    assert response.status_code == 201
+    assert new_user.email == "nuevaao@gmail.com"
+    assert new_user.id == 1
