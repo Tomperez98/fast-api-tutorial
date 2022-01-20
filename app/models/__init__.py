858b4f776a30d1ad145e2b353c22098efac4c457
Tomperez98 <tomasperezalvarez@gmail.com>
2022-01-20 17:47:59 -0500

    Stop using # noqa on init file. Replaced by all method

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
-from app.database import Base  # noqa
-from app.models.post import Post  # noqa
-from app.models.user import User  # noqa
-from app.models.vote import Vote  # noqa
+from app.database import Base
+from app.models.post import Post
+from app.models.user import User
+from app.models.vote import Vote
+
+__all__ = ["Base", "Post", "User", "Vote"]
